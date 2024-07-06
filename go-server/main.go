0be40031e7d5ca5bc8b783e5e2acb35632ef930c
darlingson <dmakuwila@gmail.com>
2024-07-06 20:07:10 +0200

    added main and the two handler functions

--- go-server/main.go
+++ go-server/main.go
@@ -20,3 +20,37 @@ var (
     nextID  = 1
     postsMu sync.Mutex
 )
+func main() {
+    http.HandleFunc("/posts", postsHandler)
+    http.HandleFunc("/posts/", postHandler)
+
+    fmt.Println("Server is running at http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func postsHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case "GET":
+        handleGetPosts(w, r)
+    case "POST":
+        handlePostPosts(w, r)
+    default:
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    }
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+    id, err := strconv.Atoi(r.URL.Path[len("/posts/"):])
+    if err != nil {
+        http.Error(w, "Invalid post ID", http.StatusBadRequest)
+        return
+    }
+
+    switch r.Method {
+    case "GET":
+        handleGetPost(w, r, id)
+    case "DELETE":
+        handleDeletePost(w, r, id)
+    default:
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    }
+}
